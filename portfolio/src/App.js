c61ec2480219c1e894aebf3a97ca5721e5795e74
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2022-10-11 04:35:04 +1300

    Deleted the header from App.js.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,4 @@
-import logo from './assets/images/logo.svg';
+// import logo from './assets/images/logo.svg';
 import './css/main.css';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -9,20 +9,6 @@ import WorkExperience from './components/work-experience';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Home />
       <Projects />
       <WorkExperience />
